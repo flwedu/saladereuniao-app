7891c2b975d041095eca5451b08d700dbeebbba6
Eduardo Aquino <edutraquino@gmail.com>
2022-01-17 18:32:02 -0300

    creating interface IHttpClient

---
+++ src/core/IHttpClient.ts
@@ -0,0 +1,10 @@
+export interface IHttpClient {
+
+    get(url: string): Promise<any>;
+
+    post(url: string, data: any): Promise<any>;
+
+    put(url: string, data: any): Promise<any>;
+
+    delete(url: string, data: any): Promise<any>;
+}

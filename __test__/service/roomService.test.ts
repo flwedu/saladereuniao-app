f705a99f43b23411a01a0c2d5e522d46c18a9ecc
Eduardo Aquino <edutraquino@gmail.com>
2022-01-17 18:48:18 -0300

    creating mock data and objects

--- __test__/service/roomService.test.ts
+++ __test__/service/roomService.test.ts
@@ -1,6 +1,22 @@
 import HttpClient from "../../src/core/HttpClient";
 import { RoomService } from "../../src/service/RoomService";
 
+const room: IRoom = {
+    id: 1,
+    name: "Test Room",
+    description: "Just a test room"
+}
+
+const rooms: IRoom[] = Array.of(room, room);
+
+// Creating mock from httpClient
+const httpClientFake: IHttpClient = {
+    get: (url: string) => Promise.resolve({ status: 200, data: rooms }),
+    post: (url: string, data: any) => Promise.resolve({ status: 200, data: room }),
+    put: (url: string, data: any) => Promise.resolve({ status: 202, data: room }),
+    delete: (url: string, data: any) => Promise.resolve({ status: 202 }),
+}
+
 it("Should return an promise when requesting for all", async () => {
 
     const httpClient = new HttpClient();

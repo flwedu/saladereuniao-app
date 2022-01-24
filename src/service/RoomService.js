9beeeaeec3eadbc1fe1c9a7de411e093c434275f
Eduardo Aquino <edutraquino@gmail.com>
2022-01-24 20:12:19 -0300

    applying class inheritance to Services

--- src/service/RoomService.js
+++ src/service/RoomService.js
@@ -1,58 +1,9 @@
 import { config } from "../config";
+import { Service } from "./Service";
 
-export class RoomService {
+export class RoomService extends Service {
   constructor(httpClient) {
-    this.httpClient = httpClient;
+    super(httpClient);
     this.baseURL = `http://localhost:${config.API_PORT}/${config.API_ROOMS}`;
-  }
-
-  listAll() {
-    return new Promise((resolve, reject) => {
-      this.httpClient
-        .get(this.baseURL)
-        .then((response) => resolve(response.data))
-        .catch((err) => reject(err));
-    });
-  }
-  findById(id) {
-    return new Promise((resolve, reject) => {
-      this.httpClient
-        .get(`${this.baseURL}/${id}`)
-        .then((response) => resolve(response.data))
-        .catch((err) => reject(err));
-    });
-  }
-  save(data) {
-    return new Promise((resolve, reject) => {
-      this.httpClient
-        .post(this.baseURL, data)
-        .then((response) => {
-          if (response.status == 201) resolve(response);
-          else reject();
-        })
-        .catch((err) => reject(err));
-    });
-  }
-  update(id, data) {
-    return new Promise((resolve, reject) => {
-      this.httpClient
-        .put(`${this.baseURL}/${id}`, data)
-        .then((response) => {
-          if (response.status == 202) resolve(response);
-          else reject();
-        })
-        .catch((err) => reject(err));
-    });
-  }
-  delete(id) {
-    return new Promise((resolve, reject) => {
-      this.httpClient
-        .delete(`${this.baseURL}/${id}`)
-        .then((response) => {
-          if (response.status == 202) resolve();
-          else reject();
-        })
-        .catch((err) => reject(err));
-    });
   }
 }

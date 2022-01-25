99f07cc718abaa3120b4f4b82bdd390946b303da
Eduardo Aquino <edutraquino@gmail.com>
2022-01-25 19:30:45 -0300

    creating VueRouter and using in app

--- src/main.js
+++ src/main.js
@@ -1,10 +1,31 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import App from "./App.vue";
+import RoomList from "./components/Room/RoomList.vue";
+import RoomEventList from "./components/RoomEvents/RoomEventList.vue";
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
 
+// Describing routes
+const routes = [
+  {
+    path: "",
+    component: RoomList,
+  },
+  {
+    path: "/events",
+    component: RoomEventList,
+  },
+];
+
+// Creating the router
+const router = new VueRouter({
+  routes,
+});
+
+// Using the router and other options in app instance
 new Vue({
   render: (h) => h(App),
+  router,
 }).$mount("#app");

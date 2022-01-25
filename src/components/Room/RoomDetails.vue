7c869425ffa21b2f5211ac5a506282206a18297d
Eduardo Aquino <edutraquino@gmail.com>
2022-01-25 20:55:42 -0300

    create roomDetais component

---
+++ src/components/Room/RoomDetails.vue
@@ -0,0 +1,63 @@
+<template>
+  <div v-if="room">
+    <form action="" method="post">
+      <div class="data">
+        <label for="name"
+          >room name:<input
+            type="text"
+            name="name"
+            v-model="room.name"
+            id="name"
+        /></label>
+        <label for="description"
+          >room description:<input
+            type="text"
+            name="description"
+            id="name"
+            v-model="room.description"
+        /></label>
+      </div>
+      <div class="buttons">
+        <input v-if="!editing" type="button" value="Edit" @click="editMode()" />
+        <input v-if="editing" type="button" value="Save" @click="save()" />
+        <input v-if="editing" type="button" value="Cancel" />
+      </div>
+    </form>
+    <room-event-list />
+  </div>
+</template>
+
+<script>
+import { HttpClient } from "../../core/HttpClient";
+import { RoomService } from "../../service/RoomService";
+import RoomEventList from "../RoomEvents/RoomEventList.vue";
+
+const httpClient = new HttpClient();
+const roomService = new RoomService(httpClient);
+
+export default {
+  components: { RoomEventList },
+  data: function () {
+    return {
+      editing: false,
+      room: {},
+    };
+  },
+  methods: {
+    editMode: function () {
+      this.editing = true;
+    },
+    save: function () {
+      this.editing = false;
+    },
+  },
+  created: function () {
+    roomService
+      .findById(this.$route.params.roomId)
+      .then((response) => (this.room = response))
+      .catch(console.error);
+  },
+};
+</script>
+
+<style></style>

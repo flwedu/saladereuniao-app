5bc9881af36953e8794fbc28ecff04dc772b0cba
Eduardo Aquino <edutraquino@gmail.com>
2022-01-25 18:51:33 -0300

    create roomEventList component

---
+++ src/components/RoomEvents/RoomEventList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="grid">
+    <room-event-card
+      v-for="roomEvent in roomEvents"
+      :key="roomEvent.id"
+      :roomEvent="roomEvent"
+    ></room-event-card>
+  </div>
+</template>
+
+<script>
+import RoomEventCard from "./RoomEventCard.vue";
+
+export default {
+  name: "room-event-list",
+  components: {
+    RoomEventCard,
+  },
+  props: {
+    roomEvents: [],
+  },
+};
+</script>
+
+<style>
+.grid {
+  display: grid;
+  grid-template-rows: 1fr 1fr 1fr;
+}
+</style>

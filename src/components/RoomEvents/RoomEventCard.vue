9fedde5138f40bd22ef875ad458d51e8a42876b4
Eduardo Aquino <edutraquino@gmail.com>
2022-01-25 18:51:13 -0300

    create roomEventCard component

---
+++ src/components/RoomEvents/RoomEventCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="card">
+    <div class="header">
+      <span>{{ roomEvent.name }}</span>
+    </div>
+    <div class="body">
+      <label for="description"
+        >Description:
+        <textarea id="description" v-text="roomEvent.description" />
+      </label>
+      <label for="startingTime"
+        >Starting at:
+        <input
+          type="datetime"
+          name="startingTime"
+          id="startingTime"
+          :value="roomEvent.startingTime"
+      /></label>
+      <label for="endingTime"
+        >Ending at:
+        <input
+          type="datetime"
+          name="endingTime"
+          id="endingTime"
+          :value="roomEvent.endingTime"
+      /></label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "room-event-card",
+  props: {
+    roomEvent: {
+      id: Number,
+      name: String,
+      description: String,
+      startingTime: Date,
+      endingTime: Date,
+      roomId: Number,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  border: 1px solid var(--color-dark);
+  border-radius: 4px;
+}
+</style>

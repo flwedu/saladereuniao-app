ce17060b7e9c0dccb520967bfc39d0ea695aca55
Eduardo Aquino <edutraquino@gmail.com>
2022-01-28 22:00:53 -0300

    remove super class Service and his dependencies

--- src/service/RoomEventService.js
+++ src/service/RoomEventService.js
@@ -1,9 +1,58 @@
 import { config } from "../config";
-import { Service } from "./Service";
 
-export class RoomEventService extends Service {
+export class RoomEventService {
   constructor(httpClient) {
-    super(httpClient);
+    this.httpClient = httpClient;
     this.baseURL = `http://localhost:${config.API_PORT}/${config.API_EVENTS}`;
+  }
+
+  listAll() {
+    return new Promise((resolve, reject) => {
+      this.httpClient
+        .get(this.baseURL)
+        .then((response) => resolve(response.data))
+        .catch((err) => reject(err));
+    });
+  }
+  findById(id) {
+    return new Promise((resolve, reject) => {
+      this.httpClient
+        .get(`${this.baseURL}/${id}`)
+        .then((response) => resolve(response.data))
+        .catch((err) => reject(err));
+    });
+  }
+  save(data) {
+    return new Promise((resolve, reject) => {
+      this.httpClient
+        .post(this.baseURL, data)
+        .then((response) => {
+          if (response.status == 201) resolve(response);
+          else reject();
+        })
+        .catch((err) => reject(err));
+    });
+  }
+  update(id, data) {
+    return new Promise((resolve, reject) => {
+      this.httpClient
+        .put(`${this.baseURL}/${id}`, data)
+        .then((response) => {
+          if (response.status == 202) resolve(response);
+          else reject();
+        })
+        .catch((err) => reject(err));
+    });
+  }
+  delete(id) {
+    return new Promise((resolve, reject) => {
+      this.httpClient
+        .delete(`${this.baseURL}/${id}`)
+        .then((response) => {
+          if (response.status == 202) resolve();
+          else reject();
+        })
+        .catch((err) => reject(err));
+    });
   }
 }
--- src/service/RoomService.js
+++ src/service/RoomService.js
@@ -1,10 +1,59 @@
 import { config } from "../config";
-import { Service } from "./Service";
 
-export class RoomService extends Service {
+export class RoomService {
   constructor(httpClient) {
-    super(httpClient);
+    this.httpClient = httpClient;
     this.baseURL = `http://localhost:${config.API_PORT}/${config.API_ROOMS}`;
+  }
+
+  listAll() {
+    return new Promise((resolve, reject) => {
+      this.httpClient
+        .get(this.baseURL)
+        .then((response) => resolve(response.data))
+        .catch((err) => reject(err));
+    });
+  }
+  findById(id) {
+    return new Promise((resolve, reject) => {
+      this.httpClient
+        .get(`${this.baseURL}/${id}`)
+        .then((response) => resolve(response.data))
+        .catch((err) => reject(err));
+    });
+  }
+  save(data) {
+    return new Promise((resolve, reject) => {
+      this.httpClient
+        .post(this.baseURL, data)
+        .then((response) => {
+          if (response.status == 201) resolve(response);
+          else reject();
+        })
+        .catch((err) => reject(err));
+    });
+  }
+  update(id, data) {
+    return new Promise((resolve, reject) => {
+      this.httpClient
+        .put(`${this.baseURL}/${id}`, data)
+        .then((response) => {
+          if (response.status == 202) resolve(response);
+          else reject();
+        })
+        .catch((err) => reject(err));
+    });
+  }
+  delete(id) {
+    return new Promise((resolve, reject) => {
+      this.httpClient
+        .delete(`${this.baseURL}/${id}`)
+        .then((response) => {
+          if (response.status == 202) resolve();
+          else reject();
+        })
+        .catch((err) => reject(err));
+    });
   }
 
   findRoomEvents(roomId, pageNumber) {

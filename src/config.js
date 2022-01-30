7d3ebffaae01ae6efbaf94884df74866a0aab0b0
Eduardo Aquino <edutraquino@gmail.com>
2022-01-30 10:14:15 -0300

    exporting the formated URL

--- src/config.js
+++ src/config.js
@@ -1,6 +1,10 @@
-export const config = {
+const config = {
   // This object contais the params connect to the Rest API
   API_PORT: 8080,
   API_ROOMS: "api/v1/rooms",
   API_EVENTS: "api/v1/events",
 };
+const URL_ROOMS = `http://localhost:${config.API_PORT}/${config.API_ROOMS}`;
+const URL_EVENTS = `http://localhost:${config.API_PORT}/${config.API_EVENTS}`;
+
+export { URL_EVENTS, URL_ROOMS };

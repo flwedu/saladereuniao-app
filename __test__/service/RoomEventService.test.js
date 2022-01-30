64eb66fb5d0b94c7ac2d4feaed809fa60f650076
Eduardo Aquino <edutraquino@gmail.com>
2022-01-30 17:20:01 -0300

    creating tests to roomEventService

---
+++ __test__/service/RoomEventService.test.js
@@ -0,0 +1,73 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import { RoomEventService } from "../../src/service/RoomEventService";
+
+// mock data
+const content = [
+  {
+    id: 1,
+    description: "Test room event",
+    name: "Test",
+    startingTime: Date.now(),
+    endingTime: Date.now(),
+    roomId: 1,
+  },
+];
+
+const httpClientFake = {
+  get: (_url) => Promise.resolve({ status: 200, data: { content } }),
+  post: (_url, _data) =>
+    Promise.resolve({
+      status: 201,
+      headers: {
+        location: "testURL",
+      },
+    }),
+  put: (_url, _data) =>
+    Promise.resolve({
+      status: 202,
+      data: {
+        id: 1,
+        name: "Updated Test Room",
+        desciption: "Just a test room",
+      },
+    }),
+  delete: (_url) => Promise.resolve({ status: 202 }),
+};
+describe("Service methods", () => {
+  const service = new RoomEventService(httpClientFake);
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("Should get a list with all room events with page number", async () => {
+    expect.assertions(2);
+    const getSpy = jest.spyOn(httpClientFake, "get");
+
+    const result = await service.listAll(1, 0);
+
+    expect(getSpy).toHaveBeenCalledTimes(1);
+    expect(result.content).toEqual(content);
+  });
+
+  it("Should get a list with all room events without pageNumber", async () => {
+    expect.assertions(2);
+    const getSpy = jest.spyOn(httpClientFake, "get");
+
+    const result = await service.listAll(1, undefined);
+
+    expect(getSpy).toHaveBeenCalledTimes(1);
+    expect(result.content).toEqual(content);
+  });
+
+  it("Should get a list with all room events without page roomId or pageNumber", async () => {
+    expect.assertions(2);
+    const getSpy = jest.spyOn(httpClientFake, "get");
+
+    const result = await service.listAll(undefined, undefined);
+
+    expect(getSpy).toHaveBeenCalledTimes(1);
+    expect(result.content).toEqual(content);
+  });
+});

d7e3b977067460c81d504bc6f1f9b2f0df0fed89
Eduardo Aquino <edutraquino@gmail.com>
2022-01-30 10:09:10 -0300

    GET list of events from service response.content

--- src/components/RoomEvents/RoomEventList.vue
+++ src/components/RoomEvents/RoomEventList.vue
@@ -1,20 +1,25 @@
 <template>
-  <div class="grid">
-    <room-event-card
-      v-for="roomEvent in roomEvents"
-      :key="roomEvent.id"
-      :roomEvent="roomEvent"
-    ></room-event-card>
-  </div>
+  <section>
+    <div class="grid" v-if="roomEvents.length">
+      <room-event-card
+        v-for="roomEvent in roomEvents"
+        :key="roomEvent.id"
+        :roomEvent="roomEvent"
+      ></room-event-card>
+    </div>
+    <div v-else>
+      <span>No events found...</span>
+    </div>
+  </section>
 </template>
 
 <script>
 import { HttpClient } from "../../core/HttpClient";
-import { RoomService } from "../../service/RoomService";
+import { RoomEventService } from "../../service/RoomEventService";
 import RoomEventCard from "./RoomEventCard.vue";
 
 const httpClient = new HttpClient();
-const roomService = new RoomService(httpClient);
+const roomEventService = new RoomEventService(httpClient);
 
 export default {
   name: "room-event-list",
@@ -28,11 +33,14 @@ export default {
     };
   },
   created: function () {
-    this.eventsPage = 0;
-    roomService
-      .findRoomEvents(this.$route.params.roomId, this.eventsPage)
+    this.eventsPage = this.$route.params.pageNumber || 0;
+
+    // If the :roomId param exists in URL, load events from this Room. But if don't exists, load all Rooms from page :pageNumber
+    // The listAll method accepts null as first arg.
+    roomEventService
+      .listAll(this.$route.params.roomId, this.eventsPage)
       .then((response) => {
-        this.roomEvents = response.data.content;
+        this.roomEvents = response.content;
       })
       .catch(console.error);
   },

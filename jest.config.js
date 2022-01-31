5d9310904f030c41a2e12f2b43dd4cdfbecec145
Eduardo Aquino <edutraquino@gmail.com>
2022-01-31 20:26:58 -0300

    add configurations

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
-  moduleFileExtensions: ["js", "jsx", "json", "vue"],
+  moduleFileExtensions: ["js", "json", "vue"],
   transform: {
     "^.+\\.vue$": "vue-jest",
+    "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
   },
+  testEnvironment: "jsdom",
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },

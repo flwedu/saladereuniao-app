45e841d3450ec36f72e35e2baf77b7a31865786f
Eduardo Aquino <edutraquino@gmail.com>
2022-01-31 20:30:33 -0300

    add test to HeaderApp component

--- __tests__/unit/components/layout/HeaderApp.spec.js
+++ __tests__/unit/components/layout/HeaderApp.spec.js
@@ -1,11 +1,11 @@
 import { shallowMount } from "@vue/test-utils";
-import HeaderApp from "components/layout/HeaderApp.vue";
+import HeaderApp from "@/components/layout/HeaderApp.vue";
 
 /* eslint-disable no-undef */
 describe("HeaderApp.vue component", () => {
   it("Component should mount", () => {
-    const wrapper = shallowMount(HeaderApp);
+    const wrapper = shallowMount(HeaderApp, {});
 
-    expect(wrapper.element).toBeTruthy();
+    expect(wrapper.find("h1").text()).toEqual("Sala de Reuniões App");
   });
 });

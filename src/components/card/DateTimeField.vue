a90d7958ef56a3dd9c342c10d1b574167ccb2587
Eduardo Aquino <edutraquino@gmail.com>
2022-02-04 17:45:21 -0300

    create component DateTimeField

---
+++ src/components/card/DateTimeField.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <span>{{ label }}</span>
+    <label class="form-field">
+      Date:
+      <input
+        type="date"
+        name="date"
+        id="date"
+        :value="localDate"
+        :readonly="editing"
+    /></label>
+    <label class="form-field">
+      Time:
+      <input
+        type="time"
+        name="time"
+        id="time"
+        :value="localTime"
+        :readonly="editing"
+      />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "date-time",
+  props: {
+    editing: Boolean,
+    label: String,
+    dateTime: [],
+  },
+  computed: {
+    localDate() {
+      const [year, month, day] = this.dateTime;
+      return new Date(year, month, day).toLocaleDateString();
+    },
+    localTime() {
+      const [, , , hour, minuts] = this.dateTime;
+      return `${hour}:${minuts}`;
+    },
+  },
+};
+</script>
+
+<style></style>

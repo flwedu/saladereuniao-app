4b47996c3290d9e2926ea4b81210f09981a51514
Eduardo Aquino <edutraquino@gmail.com>
2022-02-04 19:09:28 -0300

    created FormButtons component

---
+++ src/components/card/FormButtons.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="buttons">
+    <input
+      id="button__edit"
+      class="button"
+      v-if="!editingMode"
+      type="button"
+      value="Edit"
+      @click="enterEditingMode()"
+    />
+    <input
+      id="button__save"
+      class="button"
+      v-if="editingMode"
+      type="button"
+      value="Save"
+      @click="save()"
+    />
+    <input
+      id="button__cancel"
+      class="button"
+      v-if="editingMode"
+      type="button"
+      value="Cancel"
+      @click="cancel()"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "form-buttons",
+  props: {
+    editingMode: Boolean,
+  },
+  methods: {
+    enterEditingMode() {
+      this.$emit("enterEditingMode");
+    },
+    save() {
+      this.$emit("save");
+    },
+    cancel() {
+      this.$emit("cancel");
+    },
+  },
+};
+</script>
+
+<style>
+/* Button colors */
+#button__save {
+  background: var(--color-sucess);
+}
+
+#button__cancel {
+  background: var(--color-danger);
+}
+</style>

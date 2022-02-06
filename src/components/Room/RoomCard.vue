16b27b16a809e6c1efeeaec310d2192db6a11ac5
Eduardo Aquino <edutraquino@gmail.com>
2022-02-06 11:59:43 -0300

    move buttons styles to FormButtons file

--- src/components/Room/RoomCard.vue
+++ src/components/Room/RoomCard.vue
@@ -54,21 +54,4 @@ export default {
   grid-template-rows: 5fr 3fr;
   justify-items: center;
 }
-
-/* adjusting the separation of the buttons divs */
-.card > .buttons {
-  margin-top: 1rem;
-  display: flex;
-  justify-content: space-around;
-}
-
-.button {
-  font-weight: 500;
-  width: 6rem;
-  height: 2rem;
-  background: var(--color-main);
-  color: var(--color-text-light);
-  border: transparent 1px;
-  border-radius: 3px;
-}
 </style>
--- src/components/card/FormButtons.vue
+++ src/components/card/FormButtons.vue
@@ -48,6 +48,23 @@ export default {
 </script>
 
 <style>
+/* adjusting the separation of the buttons divs */
+.card > .buttons {
+  margin-top: 1rem;
+  display: flex;
+  justify-content: space-around;
+}
+
+.button {
+  font-weight: 500;
+  width: 6rem;
+  height: 2rem;
+  background: var(--color-main);
+  color: var(--color-text-light);
+  border: transparent 1px;
+  border-radius: 3px;
+}
+
 /* Button colors */
 #button__save {
   background: var(--color-sucess);
